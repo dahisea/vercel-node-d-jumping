be6128fae9aa3a0e770c4271117f2076c092240a
Dahi <81472122+dahisea@users.noreply.github.com>
2025-02-27 22:03:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
 export default function handler(r, s) {
-  const q = r.query, u = r.url;
-  const a = ["https://cdn-1-mod-ugf.zh-cn.eu.org", "https://cdn-r1-mod-ugf.zh-cn.eu.org", "https://cdn-r2-mod-ugf.zh-cn.eu.org"];
-  const b = ["https://gfork.dahi.icu/en", "https://gfork.dahi.icu/ja"];
-  const c = u === '/' || u === '';
-  const d = u.startsWith('/scripts/');
-  let t = c || !d ? b[Math.floor(Math.random() * b.length)] : a[Math.floor(Math.random() * a.length)] + u;
-  if (Object.keys(q).length > 0) t += '?' + new URLSearchParams(q).toString();
+  if (r.method !== 'GET') {
+    return s.status(405).send('Method Not Allowed');
+  }
+  
+  const u = r.url;
+  const x = ["https://cdn-1-mod-ugf.zh-cn.eu.org", "https://cdn-r1-mod-ugf.zh-cn.eu.org", "https://cdn-r2-mod-ugf.zh-cn.eu.org"];
+  const y = ["https://gfork.dahi.icu/en", "https://gfork.dahi.icu/ja"];
+  const p = u === '/' || u === '';
+  const q = u.startsWith('/scripts/');
+  const t = p || !q ? y[Math.floor(Math.random() * y.length)] : x[Math.floor(Math.random() * x.length)] + u;
   s.redirect(302, t);
 }
